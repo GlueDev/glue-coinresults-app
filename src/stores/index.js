372eecd22f55c2804f3a2a48da133bdef662e457
Diederik van den Burger <diederik@webrelated.nl>
2017-12-23 14:19:41 +0100

    Removed unnecessary packages and moved away from Mobx.

--- src/stores/index.js
+++
@@ -1,9 +0,0 @@
-import HistoryStore from './HistoryStore';
-import CryptosStore from './CryptosStore';
-import UserStore from './UserStore';
-
-export default {
-  history: new HistoryStore(),
-  cryptos: new CryptosStore(),
-  user:    new UserStore(),
-};
